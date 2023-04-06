f2c3f69a34a0ce1996606c3b8c6384746660f901
Karol Linkiewicz <karol@linkiewicz.pl>
2023-04-06 19:56:10 +0200

    set iri of looged user in header, display user in vue

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use ApiPlatform\Api\IriConverterInterface;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,10 +13,16 @@ class SecurityController extends AbstractController
 {
 
     #[Route('/login', name: 'app_login', methods: ['POST'])]
-    public function login(#[CurrentUser] User $user): Response
+    public function login(IriConverterInterface $iriConverter, #[CurrentUser] User $user = null): Response
     {
-        return $this->json([
-            'user' => $user ? $user->getId() : null
+        if (!$user) {
+            return $this->json([
+                'error' => 'Check if mime type is set to "application/json"',
+            ], 401);
+        }
+
+        return new Response('', 204, [
+            'Location' => $iriConverter->getIriFromResource($user),
         ]);
     }
 }
