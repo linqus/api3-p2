e9517874ceec39b7dcd5c368f9040288ae135ed5
Karol Linkiewicz <karol@linkiewicz.pl>
2023-04-08 15:13:43 +0200

    playing with jmespath syntax

--- tests/Functional/DragonTreasureResourceTest.php
+++ tests/Functional/DragonTreasureResourceTest.php
@@ -2,7 +2,10 @@
 
 namespace App\Tests\Functional;
 
+use App\Entity\DragonTreasure;
+use App\Factory\DragonTreasureFactory;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zenstruck\Browser\Json;
 use Zenstruck\Browser\Test\HasBrowser;
 use Zenstruck\Foundry\Test\ResetDatabase;
 
@@ -13,10 +16,26 @@ class DragonTreasureResourceTest extends KernelTestCase
 
     public function testGetCollectionOfTreasures(): void
     {
+
+        DragonTreasureFactory::createMany(5);
+
         $this->browser()
             ->get('/api/treasures')
             ->assertJson()
-            ->assertJsonMatches('hydra:totalItems',0)
+            ->assertJsonMatches('"hydra:totalItems"',5)
+            ->use(function (Json $json) {
+                $json->assertMatches('keys("hydra:member"[0])',[  
+                    0 => "@id",
+                    1 => "@type",
+                    2 => "name",
+                    3 => "description",
+                    4 => "value",
+                    5 => "coolFactor",
+                    6 => "owner",
+                    7 => "shortDescription",
+                    8 => "plunderedAtAgo",
+                ]);
+            })
             ;
     }
 }
